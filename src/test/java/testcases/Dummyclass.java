a37f31af4d2950aa0e72cbddc84de6fd74ac21ce
AdiLakshmiNijamala <adilakshminijamala@gmail.com>
2022-10-28 05:56:12 +0700

    Added New Class

---
+++ src/test/java/testcases/Dummyclass.java
@@ -0,0 +1,12 @@
+package testcases;
+
+public class Dummyclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Check my Tally Automation in Git");
+	
+	}
+
+}
